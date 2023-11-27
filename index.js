8589c8b031fa33d074ea8ceddf3771d2fcdfa77d
JeremiasDN25 <143090708+JeremiasDN25@users.noreply.github.com>
2023-11-27 19:24:35 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,14 @@
+function mostrarRespuestaIncorrecta() {
+    var respuestaIncorrecta = document.getElementById("respuestaIncorrecta");
+    respuestaIncorrecta.style.display = "block";
+}
+
+function mostrarRespuestaCorrecta() {
+    var respuestaCorrecta = document.getElementById("respuestaCorrecta");
+    respuestaCorrecta.style.display = "block";
+}
+
+function irASiguientePregunta() {
+    // Aquí puedes implementar la lógica para ir a la siguiente pregunta
+    // Puedes redirigir o cambiar dinámicamente el contenido de la página
+}
